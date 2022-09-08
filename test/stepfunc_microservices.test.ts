20511f8721b63ec65989913575370ad0aa36742b
“juzoace” <uzochukwunwigwe@gmail.com>
2022-09-08 16:41:14 +0100

    Initial commit

---
+++ test/stepfunc_microservices.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StepfuncMicroservices from '../lib/stepfunc_microservices-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stepfunc_microservices-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StepfuncMicroservices.StepfuncMicroservicesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
